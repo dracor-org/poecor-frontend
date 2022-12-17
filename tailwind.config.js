d01b003d0f5a5cb28925a4ecb060fc5946cdee4f
Carsten Milling <cmil@hashtable.de>
2022-12-17 15:09:58 +0100

    Add some minimal styling
    mostly taken over from dracor-org/einakter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1f2448',
+        secondary: {
+          100: '#aef',
+          200: '#08f',
+        },
+        neutral: {
+          100: '#ebf0f7',
+          200: '#1f244809',
+        },
+      },
+    },
   },
   plugins: [],
 };
