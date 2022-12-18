a0393096b032f18c70ed508de0754be6f45d5ce6
Carsten Milling <cmil@hashtable.de>
2022-12-18 15:41:27 +0100

    Add basic TopNav styles
    copied from einakter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
-test('renders welcome message', () => {
+test('renders site title', () => {
   render(
     <HelmetProvider>
       <App />
     </HelmetProvider>
   );
-  const elem = screen.getByText(/Welcome to PoeCor/i);
+  const elem = screen.getByText(/Poetry Corpora/);
   expect(elem).toBeInTheDocument();
 });
--- src/Topnav.tsx
+++ src/Topnav.tsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Topnav() {
   return (
-    <nav>
+    <nav className="flex items-center justify-between flex-wrap p-4 bg-primary text-white font-medium">
       <h1>
-        <Link to="/">PoeCor</Link>
+        <Link to="/" className="text-white hover:text-white font-light">
+          Poetry Corpora
+        </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="/corpora">Corpora</Link>
-        </li>
-      </ul>
+      <div>
+        <a
+          href="https://github.com/dracor-org/poecor-frontend"
+          title="Einakter Github"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
+        </a>
+      </div>
     </nav>
   );
 }
