ac12bd37f6d3b65ee0a56cf8f373068e1225b624
Carsten Milling <cmil@hashtable.de>
2022-12-24 13:23:56 +0100

    Add version info to TopNav

--- src/Topnav.tsx
+++ src/Topnav.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import packageInfo from '../package.json';
 
 export default function Topnav() {
   return (
@@ -8,7 +9,8 @@ export default function Topnav() {
       <h1>
         <Link to="/" className="text-white hover:text-white font-light">
           Poetry Corpora
-        </Link>
+        </Link>{' '}
+        <span className="text-xs ml-1">v{packageInfo.version}</span>
       </h1>
       <div>
         <a
